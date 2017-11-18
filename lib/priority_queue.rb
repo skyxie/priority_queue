00a752db22ad8a9e55148820246bcddf96cad35a
skyxie <skyxie@gmail.com>
2017-11-18 16:25:41 -0500

    Allow asc or desc priority queue

--- lib/priority_queue.rb
+++ lib/priority_queue.rb
@@ -2,8 +2,13 @@
 class PriorityQueue
   Node = Struct.new(:weight, :value)
 
-  def initialize k, &weight_function
+  # Enumeration for order of queue
+  ASCENDING = 0  # Lower weight = higher priority
+  DESCENDING = 1 # Higher weight = higher priority
+
+  def initialize k, order=ASCENDING, &weight_function
     @capacity = k
+    @order = order
     @weight_function = weight_function
     @nodes = []
   end
@@ -11,14 +16,24 @@ class PriorityQueue
   attr_reader :capacity
 
   def items
-    @nodes.map(&:value)
+    # List of nodes is kept in reverse order so that first node is always the limit weight
+    # Reverse order on output
+    @nodes.map(&:value).reverse
   end
 
   def full?
     @nodes.size == @capacity
   end
 
-  def max_weight
+  def asc?
+    @order == ASCENDING
+  end
+
+  def desc?
+    @order == DESCENDING
+  end
+
+  def limit_weight
     @nodes.empty? ? 0 : @nodes.first.weight
   end
 
@@ -29,35 +44,36 @@ class PriorityQueue
     new_node = Node.new(weight, value)
 
     if !full?
-      @nodes = PriorityQueue.insert new_node, @nodes
-    elsif weight < max_weight
-      # The list is at capacity so drop the first and largest element
+      @nodes = PriorityQueue.insert new_node, @nodes, @order
+    elsif (asc? && weight < limit_weight) || (desc? && weight > limit_weight)
+      # The list is at capacity so drop the first element and add new element
       @nodes.shift
-      @nodes = PriorityQueue.insert new_node, @nodes
+      @nodes = PriorityQueue.insert new_node, @nodes, @order
     end
   end
 
-  def self.insert node, list
+  def self.insert node, list, order
     size = list.size
 
-    return [node] if size == 0
+    return [node] if size == 0 # Edge case of empty list
 
     # Split the list in half and merge the result
     # For odd numbers, skew to the right
     left = list[0, size / 2]
     right = list[(size / 2)..-1]
 
-    if right.first.weight >= node.weight
+    if (order == ASCENDING && right.first.weight >= node.weight) ||
+        (order == DESCENDING && right.first.weight <= node.weight)
       # Leaf case where there is only 1 element in the list
       if right.size == 1
         left + right + [node]
       else
-        left + insert(node, right)
+        left + insert(node, right, order)
       end
     elsif left.empty?
       [node] + right
     else
-      insert(node, left) + right
+      insert(node, left, order) + right
     end
   end
 end
--- spec/priority_queue_spec.rb
+++ spec/priority_queue_spec.rb
@@ -12,68 +12,103 @@ RSpec.describe(PriorityQueue) do
     end
   end
 
-  describe "when there are fewer than k values" do
-    let(:queue) { PriorityQueue.new(3) { |x| x } }
+  describe "when order is descending" do
+    let(:capacity) { 5 }
+    let(:order) { PriorityQueue::DESCENDING }
+    let(:weight_function) { -> (x) { x } }
 
-    before { 2.times { |i| queue.add i } }
+    let(:queue) { PriorityQueue.new(capacity, order, &weight_function) }
 
-    it 'should have all values in order' do
-      expect(queue.items).to eql([1, 0])
-    end
-  end
+    let(:values) { [10, 14, 3, 9, 8, 4, 0, 2, 19, 1] }
 
-  describe "when there are more than k values" do
-    let(:queue) { PriorityQueue.new(5) { |x| x } }
+    before { values.each { |i| queue.add i } }
 
-    before do
-      [10, 14, 3, 9, 8, 4, 0, 2, 19, 1].each { |i| queue.add i }
-    end
+    describe "when there are fewer than k values" do
+      let(:values) { [0, 1] }
 
-    it 'should only have k values of lowest weight' do
-      expect(queue.items).to eql([4, 3, 2, 1, 0])
+      it 'should have all values in order' do
+        expect(queue.items).to eql([1, 0])
+      end
     end
 
-    describe "when all values have identical weight" do
-      let(:queue) { PriorityQueue.new(3) { |x| 1 } }
+    describe "when there are more than k values" do
+      it 'should only have k values of lowest weight' do
+        expect(queue.items).to eql(values.sort.reverse.slice(0, capacity))
+      end
 
-      it 'should fill to capacity and stop adding elements' do
-        expect(queue.items).to eql([10, 14, 3])
+      describe "when all values have identical weight" do
+        let(:capacity) { 3 }
+        let(:weight_function) { -> (x) { 1 } }
+
+        it 'should fill to capacity and stop adding elements' do
+          expect(queue.items).to eql(values.slice(0, capacity).reverse)
+        end
       end
     end
   end
 
-  describe "find closest k distances" do
-    let(:sol) { [5.0e-06,0.0,0.0] }
+  describe "when order is ascending" do
+    let(:order) { PriorityQueue::ASCENDING }
+    let(:weight_function) { -> (x) { x } }
+    let(:capacity) { 5 }
 
-    let(:queue) do
-      PriorityQueue.new(3) do |x|
-        sol.zip(x).reduce(0) do |dist, (a, b)|
-          dist + (a - b) ** 2
+    let(:queue) { PriorityQueue.new(capacity, order, &weight_function) }
+
+    let(:values) { [10, 14, 3, 9, 8, 4, 0, 2, 19, 1] }
+
+    before { values.each { |i| queue.add i } }
+
+    describe "when there are fewer than k values" do
+      let(:values) { [0, 1] }
+
+      it 'should have all values in order' do
+        expect(queue.items).to eql([0, 1])
+      end
+    end
+
+    describe "when there are more than k values" do
+      it 'should only have k values of lowest weight' do
+        expect(queue.items).to eql(values.sort.slice(0, capacity))
+      end
+
+      describe "when all values have identical weight" do
+        let(:capacity) { 3 }
+        let(:weight_function) { -> (x) { 1 } }
+
+        it 'should fill to capacity and stop adding elements' do
+          expect(queue.items).to eql(values.slice(0, capacity).reverse)
         end
       end
     end
 
-    before do
-      [
-        [219.740502,0.003449,4.177065],
-        [45.210918,0.003365,-16.008996],
-        [344.552785,0.030213,277.614965],
-        [82.835513,0.012476,-105.61954],
-        [195.714261,0.034068,-167.695291],
-        [54.905296,0.017912,3.787796],
-        [54.367897,0.020886,19.827115],
-        [180.654532,0.086213,87.668389]
-      ].each do |x|
-        queue.add x
+    describe "find closest k distances" do
+      let(:weight_function) do
+        sol = [5.0e-06,0.0,0.0]
+        -> (x) {
+          sol.zip(x).reduce(0) do |dist, (a, b)|
+            dist + (a - b) ** 2
+          end
+        }
       end
-    end
 
-    it "should find the k closest points" do
-      expect(queue.items).to eql([
-        [54.367897,0.020886,19.827115],
-        [54.905296,0.017912,3.787796],
-        [45.210918,0.003365,-16.008996]
-      ])
+      let(:capacity) { 3 }
+
+      let(:values) do
+        [
+          [219.740502,0.003449,4.177065],
+          [45.210918,0.003365,-16.008996],
+          [344.552785,0.030213,277.614965],
+          [82.835513,0.012476,-105.61954],
+          [195.714261,0.034068,-167.695291],
+          [54.905296,0.017912,3.787796],
+          [54.367897,0.020886,19.827115],
+          [180.654532,0.086213,87.668389]
+        ]
+      end
+
+      it "should find the k closest points" do
+        expect(queue.items).to eql([values[1], values[5], values[6]])
+      end
     end
   end
 end
